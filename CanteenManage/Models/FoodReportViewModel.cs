6af99d9a324937ef5c3f887a5db43ddc15399b59
Abinashbls <abinashd@esspl.com>
2025-06-04 10:19:12 +0530

    pop

---
+++ CanteenManage/Models/FoodReportViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CanteenManage.CanteenRepository.Models;
+
+namespace CanteenManage.Models
+{
+    public class FoodReportViewModel : LayoutViewDataModel
+    {
+        public List<FoodOrder> FoodOrders { get; set; }
+    }
+}
